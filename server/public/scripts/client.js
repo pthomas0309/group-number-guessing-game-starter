c858ec29b9bc9db21087fb9a05b645c3523f4938
pthomas0309 <preston.thomas355@gmail.com>
2021-05-20 14:59:38 -0500

    server and client side structure
    test faze

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,59 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  // handle the click button
+  $( '#submitBtn' ).on('click', addGuess);
+  // get request
+  getRequest();
+}
+
+function addGuess(){
+  //gather input values
+  let newGuess={
+    playerOne: $('#playerOneIn').val(),
+    playerTwo: $('#playerTwoIn').val(),
+    playerThree: $('#playerThreeIn').val(),
+    playerFour: $('#playerFourIn').val()
+  }
+  // add to array,  which is in the server.js files
+  // push it to that array
+  // make a post request with newGuess -> ajax
+  $.ajax({
+    url: '/guesses',
+    method: 'POST',
+    data: newGuess
+  }).then(response => {
+    console.log(response); // testing for response values
+    getRequest();
+  }) // end .then function
+
+  //empty inputs
+  $('#playerOneIn').val(''),
+  $('#playerTwoIn').val(''),
+  $('#playerThreeIn').val(''),
+  $('#playerFourIn').val('')
+} // end function addGuess
+
+
+// GET Route for displaying info to DOM
+function getRequest(){
+  console.log('in getRequest function')
+  // go to server route /guesses
+  $.ajax({
+    method: 'GET',
+    url: '/guesses'
+  }).then(function (response){
+    console.log(response);
+    //empty DOM
+    $('#guess').empty();
+  // append guesses to the DOM
+  for (let guess of response){
+    $('#guesses').append(`
+      <li>Player 1\'s guess: ${guess.playerOne}</li>
+      <li>Player 2\'s guess: ${guess.playerTwo}</li>
+      <li>Player 3\'s guess: ${guess.playerThree}</li>
+      <li>Player 4\'s guess: ${guess.playerFour}</li>
+      `)
+    }
+  })
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+// empty array to use later
+let newGuesses = [];
+//numbers randomly generated
+let answer = randomNumber(1,25);
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -11,7 +18,36 @@ app.use(express.static('server/public'));
 
 // GET & POST Routes go here
 
+// create a get route to retrieve data upon user request
+app.get('/new-guesses', (req, res) => {
+  // console.log to make sure that it is working
+  console.log('Here are the /newGuesses');
+  // send response back to client so that they can view the data that they are requesting
+  res.send(newGuesses);
+});
+
+// create a get route to randomly generate a number
+app.get('/random-answers', (req, res) => {
+  //call the random number generator
+  //log the random number
+  console.log('Correct answer', answer);
+  //send data to a client 
+  res.send(String(answer));
+});
+
+
+
+//POST ROUTE FOR USER INPUT TO THE GAME 
+app.post('/new-guesses', (req,res) => {
+  //add incoming guesses
+  console.log(req.body);
+  newGuesses.push(req.body);
+  //send good response
+  res.sendStatus(201)
+});
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
-})
+});
+
+
